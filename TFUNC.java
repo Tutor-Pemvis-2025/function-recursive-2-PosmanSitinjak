f02e496f0958a6f8c98ca599ce6b23f241ba070a
posmansitinjak06-creator <posmansitinjak06@gmail.com>
2025-11-11 20:53:33 +0700

    Posman
    Selesai

---
+++ TFUNC.java
@@ -0,0 +1,28 @@
+// Posman H.C. Sitinjak 12S25048
+
+import java.util.*;
+import java.lang.Math;
+
+public class tutor_recution {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n, hasil;
+
+        n = Integer.parseInt(input.nextLine());
+        hasil = pertambahan(n);
+        System.out.println(hasil);
+    }
+    
+    public static int pertambahan(int n) {
+        int hasil;
+
+        if (n == 1) {
+            hasil = 1;
+        } else {
+            hasil = pertambahan(n - 1) + n;
+        }
+        
+        return hasil;
+    }
+}
